a0da8a8eddde910122cfc81a3e5b8fb00c08406b
Andy Green <andy@warmcat.com>
2010-11-08 17:12:19 +0000

    add-COPYING-lgpl-2.1.patch
    Signed-off-by: Andy Green <andy@warmcat.com>

--- test-server/test-server.c
+++ test-server/test-server.c
@@ -1,3 +1,24 @@
+/*
+ * libwebsockets-test-server - libwebsockets test implementation
+ * 
+ * Copyright (C) 2010 Andy Green <andy@warmcat.com>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation:
+ *  version 2.1 of the License.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA  02110-1301  USA
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,14 +26,6 @@
 #include <string.h>
 
 #include "../lib/libwebsockets.h"
-
-/*
- * libwebsocket Example server  Copyright 2010 Andy Green <andy@warmcat.com>
- * 
- * Licensed under GPL2
- * 
- * Shows how to use libwebsocket 
- */
 
 #define LOCAL_RESOURCE_PATH "/usr/share/libwebsockets-test-server"
 static int port = 7681;
