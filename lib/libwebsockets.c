4c38f14527682dbdc18b9dd32ca8d0c00775783c
David Galeano <davidgaleano@turbulenz.biz>
2013-01-09 19:49:50 +0800

    Allow extensions when no protocol was specified.

--- lib/libwebsockets.c
+++ lib/libwebsockets.c
@@ -1213,7 +1213,7 @@ select_protocol:
 		wsi->c_callback = wsi->protocol->callback;
 		free(wsi->c_protocol);
 
-		goto check_accept;
+		goto check_extensions;
 	}
 
 	while (*pc && !okay) {
@@ -1264,6 +1264,8 @@ select_protocol:
 		goto bail2;
 	}
 
+
+check_extensions:
 
 	/* instantiate the accepted extensions */
 
