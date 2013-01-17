4a673a38cadd452318609b81dc1270d8cdb6b6de
Andy Green <andy.green@linaro.org>
2013-01-17 19:53:16 +0800

    deprecate x google mux
    Unfortunately this code is beginning to rot due to lack of demand to
    provide it and it being disabled by default.
    
    If demand appears we can revert this and resume work on it, otherwise
    let's bite the bullet for the moment.
    
    Signed-off-by: Andy Green <andy.green@linaro.org>

--- lib/extension.c
+++ lib/extension.c
@@ -2,16 +2,8 @@
 
 #include "extension-deflate-frame.h"
 #include "extension-deflate-stream.h"
-#include "extension-x-google-mux.h"
 
 struct libwebsocket_extension libwebsocket_internal_extensions[] = {
-#ifdef LWS_EXT_GOOGLE_MUX
-	{
-		"x-google-mux",
-		lws_extension_callback_x_google_mux,
-		sizeof (struct lws_ext_x_google_mux_conn)
-	},
-#endif
 #ifdef LWS_EXT_DEFLATE_STREAM
 	{
 		"deflate-stream",
