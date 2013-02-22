b37827b651497ef936dd17df03fd7168af06a43e
Joakim Soderberg <joakim.soderberg@gmail.com>
2013-02-22 09:28:08 +0800

    Added CPack support + some more.
    - "make dist" will now produce a tar.gz file. This includes everything (not really what we want but a start).
    - Got rid of a bunch of defines and variables that aren't used.
    - Added the option LINK_TESTAPPS_DYNAMIC that enables linking to the shared library version of the lib for the test apps, instead of doing it statically like the default is.
    - Fixed proper support for the --prefix stuff (-DCMAKE_INSTALL_PREFIX for cmake). (Don't specify /usr/local explicitly, that is the default anyway and will break other platforms).
    
    - Note: I noticed a problem with the "INSTALL_DATADIR" define used by the test-apps. Since we hard code the path to the certs using this, doing "DESTDIR=/bla make install" will result in not being able to use the SSL mode for the test-apps since they won't find the certs. (This also applies to the autoconf project). Fixed this by setting "../share" as the default location instead of using the prefix.

--- config.h.cmake
+++ config.h.cmake
@@ -124,27 +124,6 @@
    */
 #undef LT_OBJDIR // We're not using libtool
 
-/* Name of package */
-#cmakedefine PACKAGE
-
-/* Define to the address where bug reports for this package should be sent. */
-#cmakedefine PACKAGE_BUGREPORT
-
-/* Define to the full name of this package. */
-#cmakedefine PACKAGE_NAME
-
-/* Define to the full name and version of this package. */
-#cmakedefine PACKAGE_STRING
-
-/* Define to the one symbol short name of this package. */
-#cmakedefine PACKAGE_TARNAME
-
-/* Define to the home page for this package. */
-#cmakedefine PACKAGE_URL
-
-/* Define to the version of this package. */
-#cmakedefine PACKAGE_VERSION
-
 /* Define to 1 if you have the ANSI C header files. */
 #cmakedefine STDC_HEADERS
 
