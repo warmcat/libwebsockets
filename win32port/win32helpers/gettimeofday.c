e5ae9a95fb2c95b7df47f2a43d58225e667a8ada
Arokux B <arokux@gmail.com>
2013-02-28 09:18:38 +0800

    fix win32 gettimeofday for mingw
    here are the problems that I found while compiling libwebsockets with
    CMake using as a generator MinGW Makefiles.
    
    1. In http://git.warmcat.com/cgi-bin/cgit/libwebsockets/plain/win32port/win32helpers/gettimeofday.c
    
    Remove spaces around time.h, otherwise it looks for " time.h " and
    fails with "No such file ..."
    
    I also needed to comment out
    
    struct timezone
    {
      int  tz_minuteswest; /* minutes W of Greenwich */
      int  tz_dsttime;     /* type of dst correction */
    };
    
    It is already declared in mingw's time.h
    
    Signed-off-by: Arokux B <arokux@gmail.com>

--- win32port/win32helpers/gettimeofday.c
+++ win32port/win32helpers/gettimeofday.c
@@ -1,16 +1,22 @@
-#include < time.h >
+#include <time.h>
 #include <windows.h> //I've ommited context line.
 #if defined(_MSC_VER) || defined(_MSC_EXTENSIONS)
   #define DELTA_EPOCH_IN_MICROSECS  11644473600000000Ui64
 #else
   #define DELTA_EPOCH_IN_MICROSECS  11644473600000000ULL
 #endif
- 
+
+#ifdef __MINGW32__
+#else
+ #ifdef __MINGW64__
+ #else
 struct timezone 
 {
   int  tz_minuteswest; /* minutes W of Greenwich */
   int  tz_dsttime;     /* type of dst correction */
 };
+ #endif
+#endif
  
 int gettimeofday(struct timeval *tv, struct timezone *tz)
 {
