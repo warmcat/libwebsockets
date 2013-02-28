fc90f698b3dc67e41295c77444660bb5c16f946a
Andy Green <andy.green@linaro.org>
2013-02-28 09:22:38 +0800

    normalize formatting in gettimeofday
    Signed-off-by: Andy Green <andy.green@linaro.org>

--- win32port/win32helpers/gettimeofday.c
+++ win32port/win32helpers/gettimeofday.c
@@ -1,5 +1,6 @@
 #include <time.h>
-#include <windows.h> //I've ommited context line.
+#include <windows.h> //I've ommited context line
+
 #if defined(_MSC_VER) || defined(_MSC_EXTENSIONS)
   #define DELTA_EPOCH_IN_MICROSECS  11644473600000000Ui64
 #else
@@ -12,43 +13,40 @@
  #else
 struct timezone 
 {
-  int  tz_minuteswest; /* minutes W of Greenwich */
-  int  tz_dsttime;     /* type of dst correction */
+	int  tz_minuteswest;	/* minutes W of Greenwich */
+	int  tz_dsttime;	/* type of dst correction */
 };
  #endif
 #endif
- 
+
 int gettimeofday(struct timeval *tv, struct timezone *tz)
 {
-  FILETIME ft;
-  unsigned __int64 tmpres = 0;
-  static int tzflag;
- 
-  if (NULL != tv)
-  {
-	GetSystemTimeAsFileTime(&ft);
- 
-	tmpres |= ft.dwHighDateTime;
-	tmpres <<= 32;
-	tmpres |= ft.dwLowDateTime;
- 
-	/*converting file time to unix epoch*/
-	tmpres -= DELTA_EPOCH_IN_MICROSECS; 
-	tmpres /= 10;  /*convert into microseconds*/
-	tv->tv_sec = (long)(tmpres / 1000000UL);
-	tv->tv_usec = (long)(tmpres % 1000000UL);
-  }
- 
-  if (NULL != tz)
-  {
-	if (!tzflag)
-	{
-	  _tzset();
-	  tzflag++;
+	FILETIME ft;
+	unsigned __int64 tmpres = 0;
+	static int tzflag;
+
+	if (NULL != tv) {
+		GetSystemTimeAsFileTime(&ft);
+
+		tmpres |= ft.dwHighDateTime;
+		tmpres <<= 32;
+		tmpres |= ft.dwLowDateTime;
+
+		/*converting file time to unix epoch*/
+		tmpres -= DELTA_EPOCH_IN_MICROSECS; 
+		tmpres /= 10;  /*convert into microseconds*/
+		tv->tv_sec = (long)(tmpres / 1000000UL);
+		tv->tv_usec = (long)(tmpres % 1000000UL);
 	}
-	tz->tz_minuteswest = _timezone / 60;
-	tz->tz_dsttime = _daylight;
-  }
  
-  return 0;
+	if (NULL != tz) {
+		if (!tzflag) {
+			_tzset();
+			tzflag++;
+		}
+		tz->tz_minuteswest = _timezone / 60;
+		tz->tz_dsttime = _daylight;
+	}
+
+	return 0;
 }
