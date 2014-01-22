010a7e5123a204eecf0a99e86fa35ecdd5767971
martell <martellmalone@gmail.com>
2014-01-22 18:25:53 +0000

    fix mingw64 builds

--- win32port/win32helpers/gettimeofday.h
+++ win32port/win32helpers/gettimeofday.h
@@ -15,13 +15,15 @@
 #else
   #define DELTA_EPOCH_IN_MICROSECS  11644473600000000ULL
 #endif
- 
+
+#ifndef _TIMEZONE_DEFINED 
 struct timezone 
 {
   int  tz_minuteswest; /* minutes W of Greenwich */
   int  tz_dsttime;     /* type of dst correction */
 };
- 
+#endif
+
 int gettimeofday(struct timeval *tv, struct timezone *tz);
 
 
