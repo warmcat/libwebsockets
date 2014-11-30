66a5faa633d5e991b2ce26d96923987576097d97
Andy Green <andy.green@linaro.org>
2014-11-30 12:48:22 +0800

    coverity 83671 dead code
    Signed-off-by: Andy Green <andy.green@linaro.org>

--- lib/lws-plat-unix.c
+++ lib/lws-plat-unix.c
@@ -356,8 +356,6 @@ interface_to_sa(struct libwebsocket_context *context,
 			break;
 #ifdef LWS_USE_IPV6
 		case AF_INET6:
-			if (rc >= 0)
-				break;
 			memcpy(&addr6->sin6_addr,
 			  &((struct sockaddr_in6 *)ifc->ifa_addr)->sin6_addr,
 						       sizeof(struct in6_addr));
