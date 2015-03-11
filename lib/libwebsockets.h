ec86b4e6c4b702f16e1de944ebb2eb1bfe4991a1
Andy Green <andy.green@linaro.org>
2015-03-11 08:35:15 +0800

    documentation add info http_proxy members
    From https://libwebsockets.org/trac/libwebsockets/ticket/104
    
    Signed-off-by: Andy Green <andy.green@linaro.org>

--- lib/libwebsockets.h
+++ lib/libwebsockets.h
@@ -1028,6 +1028,8 @@ struct libwebsocket_extension {
  * @ssl_cipher_list:	List of valid ciphers to use (eg,
  * 			"RC4-MD5:RC4-SHA:AES128-SHA:AES256-SHA:HIGH:!DSS:!aNULL"
  * 			or you can leave it as NULL to get "DEFAULT"
+ * @http_proxy_address: If non-NULL, attempts to proxy via the given address
+ * @http_proxy_port:	If http_proxy_address was non-NULL, uses this port at the address 
  * @gid:	group id to change to after setting listen socket, or -1.
  * @uid:	user id to change to after setting listen socket, or -1.
  * @options:	0, or LWS_SERVER_OPTION_DEFEAT_CLIENT_MASK
