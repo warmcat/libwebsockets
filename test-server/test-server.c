a65220c6c740814d93f002d43e1646d0fff9cb6a
Roger A. Light <roger@atchoo.org>
2015-05-10 21:45:06 +0100

    Don't leak internal macros into public in lws_config.h.

--- test-server/test-server.c
+++ test-server/test-server.c
@@ -18,8 +18,6 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  *  MA  02110-1301  USA
  */
-#include "lws_config.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
