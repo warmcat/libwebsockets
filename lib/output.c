warmcat/libwebsockets

commit 5e203f78e21ee3313d763d8ee6d499fa0163f51a

output allow LWS_PRE+4 on top of rx_buffer_size for max send chunk
https://github.com/warmcat/libwebsockets/issues/522

Signed-off-by: Andy Green <andy@warmcat.com>

lib/output.c
@@ -127,7 +127,7 @@ int lws_issue_raw(struct lws *wsi, unsigned char *buf, size_t len)
 		lwsl_warn("** error invalid sock but expected to send\n");
 
 	/* limit sending */
-	n = wsi->protocol->rx_buffer_size;
+	n = wsi->protocol->rx_buffer_size + LWS_PRE + 4;
 	if (!n)
 		n = LWS_MAX_SOCKET_IO_BUF;
 	if (n > len)

commit fa34a82ca1284499200a2c0e73a2aef231d71568

output size trimming with default rxbuf fix
5e203f78e21ee3313d763d8ee6d499fa0163f51a accidentally
trashed the "zero rz_buffer_size means 4096" handling
for tx chunking.

Signed-off-by: Andy Green <andy@warmcat.com>

lib/output.c
@@ -127,9 +127,10 @@ int lws_issue_raw(struct lws *wsi, unsigned char *buf, size_t len)
 		lwsl_warn("** error invalid sock but expected to send\n");
 
 	/* limit sending */
-	n = wsi->protocol->rx_buffer_size + LWS_PRE + 4;
+	n = wsi->protocol->rx_buffer_size;
 	if (!n)
 		n = LWS_MAX_SOCKET_IO_BUF;
+	n += LWS_PRE + 4;
 	if (n > len)
 		n = len;
 