eda447e74a1eef42bdc839cadde5a6c7ea6efa49
Martin C Drohmann <martin@connectedsignals.com>
2016-06-01 12:12:44 -0700

    Revert changes in daemonize.c from commit 22d6f39e7fb270d0c011bae820d2f8adc8b19061

--- lib/daemonize.c
+++ lib/daemonize.c
@@ -142,9 +142,20 @@ lws_daemonize(const char *_lock_path)
 		exit(9);
 	}
 
-	/* If we got a good PID, then we can exit the parent process. */
-	if (pid_daemon > 0)
-		exit(0);
+        /* If we got a good PID, then we can exit the parent process. */
+	if (pid_daemon > 0) {
+
+               /*
+                * Wait for confirmation signal from the child via
+                * SIGCHILD / USR1, or for two seconds to elapse
+                * (SIGALRM).  pause() should not return.
+                */
+               alarm(2);
+
+               pause();
+               /* should not be reachable */
+               exit(1);
+       }
 
 	/* At this point we are executing as the child process */
 	parent = getppid();
