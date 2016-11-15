bfffba9e09080af34200ede1857e73153cbd905f
Andy Green <andy@warmcat.com>
2016-11-15 17:01:19 +0800

    client: protect againt losing ah by lws_client_connect_2

--- lib/client-handshake.c
+++ lib/client-handshake.c
@@ -19,6 +19,11 @@ lws_client_connect_2(struct lws *wsi)
 
 	lwsl_client("%s\n", __func__);
 
+	if (!wsi->u.hdr.ah) {
+		lwsl_err("ah was NULL at cc2\n");
+		goto oom4;
+	}
+
 	/* proxy? */
 
 	if (context->http_proxy_port) {
