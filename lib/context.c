d766c99861318be398cc763430d2ca866b662903
Andy Green <andy@warmcat.com>
2017-07-27 07:26:00 +0800

    dummy handler: LWS_CALLBACK_HTTP_FILE_COMPLETION

--- lib/context.c
+++ lib/context.c
@@ -237,6 +237,12 @@ lws_callback_http_dummy(struct lws *wsi, enum lws_callback_reasons reason,
 #endif
 			return -1;
 		break;
+#if !defined(LWS_NO_SERVER)
+	case LWS_CALLBACK_HTTP_FILE_COMPLETION:
+		if (lws_http_transaction_completed(wsi))
+			return -1;
+		break;
+#endif
 
 	case LWS_CALLBACK_HTTP_WRITEABLE:
 #ifdef LWS_WITH_CGI
