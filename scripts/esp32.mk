c2abf59c688fc820b545c52dd38714862f9e7f7e
Andy Green <andy@warmcat.com>
2017-08-19 08:14:49 +0800

    esp32: align build system for esp-idf v3

--- scripts/esp32.mk
+++ scripts/esp32.mk
@@ -10,7 +10,9 @@ SHELL=/bin/bash
 
 ESPPORT ?= $(CONFIG_ESPTOOLPY_PORT)
 
-jbi=$(COMPONENT_PATH)/../build/json-buildinfo
+LWS_BUILD_PATH=$(PROJECT_PATH)/build
+
+jbi=$(LWS_BUILD_PATH)/json-buildinfo
 
 FAC=$(CONFIG_LWS_IS_FACTORY_APPLICATION)
 ifeq ($(FAC),)
@@ -19,20 +21,20 @@ endif
 export FAC
 DIRNAME:=$(shell basename $$(pwd) | tr -d '\n')
 
-$(COMPONENT_PATH)/../build/pack.img: $(APP_BIN)
+$(LWS_BUILD_PATH)/pack.img: $(APP_BIN)
 	GNUSTAT=stat ;\
 	if [ `which gstat 2>/dev/null` ] ; then GNUSTAT=gstat ; fi ;\
 	DIRNAME=$$(basename $$(pwd) | tr -d '\n') ;\
-	genromfs -f $(COMPONENT_PATH)/../build/romfs.img -d $(COMPONENT_PATH)/../romfs-files ; \
-        RLEN=$$($$GNUSTAT -c %s $(COMPONENT_PATH)/../build/romfs.img) ;\
-        LEN=$$($$GNUSTAT -c %s $(COMPONENT_PATH)/../build/$$DIRNAME.bin) ;\
+	genromfs -f $(LWS_BUILD_PATH)/romfs.img -d $(PROJECT_PATH)/romfs-files ; \
+        RLEN=$$($$GNUSTAT -c %s $(LWS_BUILD_PATH)/romfs.img) ;\
+        LEN=$$($$GNUSTAT -c %s $(LWS_BUILD_PATH)/$$DIRNAME.bin) ;\
         printf "             Original length: 0x%06x (%8d)\n" $$LEN $$LEN ; \
-        printf %02x $$(( $$RLEN % 256 )) | xxd -r -p >> $(COMPONENT_PATH)/../build/$$DIRNAME.bin ;\
-        printf %02x $$(( ( $$RLEN / 256 ) % 256 )) | xxd -r -p >> $(COMPONENT_PATH)/../build/$$DIRNAME.bin ;\
-        printf %02x $$(( ( $$RLEN / 65536 ) % 256 )) | xxd -r -p >> $(COMPONENT_PATH)/../build/$$DIRNAME.bin ;\
-        printf %02x $$(( ( $$RLEN / 16777216 ) % 256 )) | xxd -r -p >> $(COMPONENT_PATH)/../build/$$DIRNAME.bin ;\
-        cat $(COMPONENT_PATH)/../build/romfs.img >>$(COMPONENT_PATH)/../build/$$DIRNAME.bin ; \
-        LEN=$$($$GNUSTAT -c %s $(COMPONENT_PATH)/../build/$$DIRNAME.bin) ;\
+        printf %02x $$(( $$RLEN % 256 )) | xxd -r -p >> $(LWS_BUILD_PATH)/$$DIRNAME.bin ;\
+        printf %02x $$(( ( $$RLEN / 256 ) % 256 )) | xxd -r -p >> $(LWS_BUILD_PATH)/$$DIRNAME.bin ;\
+        printf %02x $$(( ( $$RLEN / 65536 ) % 256 )) | xxd -r -p >> $(LWS_BUILD_PATH)/$$DIRNAME.bin ;\
+        printf %02x $$(( ( $$RLEN / 16777216 ) % 256 )) | xxd -r -p >> $(LWS_BUILD_PATH)/$$DIRNAME.bin ;\
+        cat $(LWS_BUILD_PATH)/romfs.img >> $(LWS_BUILD_PATH)/$$DIRNAME.bin ; \
+        LEN=$$($$GNUSTAT -c %s $(LWS_BUILD_PATH)/$$DIRNAME.bin) ;\
 	UNIXTIME=$$(date +%s | tr -d '\n') ; \
 	echo -n -e "{\r\n \"schema\": \"lws1\",\r\n \"model\": \"$(CONFIG_LWS_MODEL_NAME)\",\r\n \"builder\": \"" > $(jbi) ;\
 	hostname | tr -d '\n' >> $(jbi) ;\
@@ -50,14 +52,14 @@ $(COMPONENT_PATH)/../build/pack.img: $(APP_BIN)
 	echo -n -e "\",\r\n \"factory\": \"$(FAC)" >> $(jbi) ;\
 	echo -n -e "\"\r\n}"  >> $(jbi) ;\
 	JLEN=$$($$GNUSTAT -c %s $(jbi)) ;\
-	printf %02x $$(( $$JLEN % 256 )) | xxd -r -p >> $(COMPONENT_PATH)/../build/$$DIRNAME.bin ;\
-	printf %02x $$(( ( $$JLEN / 256 ) % 256 )) | xxd -r -p >> $(COMPONENT_PATH)/../build/$$DIRNAME.bin ;\
-	printf %02x $$(( ( $$JLEN / 65536 ) % 256 )) | xxd -r -p >> $(COMPONENT_PATH)/../build/$$DIRNAME.bin ;\
-	printf %02x $$(( ( $$JLEN / 16777216 ) % 256 )) | xxd -r -p >> $(COMPONENT_PATH)/../build/$$DIRNAME.bin ;\
-	cat $(jbi) >> $(COMPONENT_PATH)/../build/$$DIRNAME.bin ;\
-	cp $(COMPONENT_PATH)/../build/$$DIRNAME.bin $(COMPONENT_PATH)/../build/pack.img ;\
-        LEN=$$($$GNUSTAT -c %s $(COMPONENT_PATH)/../build/$$DIRNAME.bin) ;\
-	cp $(COMPONENT_PATH)/../build/$$DIRNAME.bin $(COMPONENT_PATH)/../build/$$DIRNAME-$$UNIXTIME.bin ;\
+	printf %02x $$(( $$JLEN % 256 )) | xxd -r -p >> $(LWS_BUILD_PATH)/$$DIRNAME.bin ;\
+	printf %02x $$(( ( $$JLEN / 256 ) % 256 )) | xxd -r -p >> $(LWS_BUILD_PATH)/$$DIRNAME.bin ;\
+	printf %02x $$(( ( $$JLEN / 65536 ) % 256 )) | xxd -r -p >> $(LWS_BUILD_PATH)/$$DIRNAME.bin ;\
+	printf %02x $$(( ( $$JLEN / 16777216 ) % 256 )) | xxd -r -p >> $(LWS_BUILD_PATH)/$$DIRNAME.bin ;\
+	cat $(jbi) >> $(LWS_BUILD_PATH)/$$DIRNAME.bin ;\
+	cp $(LWS_BUILD_PATH)/$$DIRNAME.bin $(LWS_BUILD_PATH)/pack.img ;\
+        LEN=$$($$GNUSTAT -c %s $(LWS_BUILD_PATH)/$$DIRNAME.bin) ;\
+	cp $(LWS_BUILD_PATH)/$$DIRNAME.bin $(LWS_BUILD_PATH)/$$DIRNAME-$$UNIXTIME.bin ;\
 	printf "    After ROMFS + Build info: 0x%06x (%8d)\n" $$LEN $$LEN
 
 .PHONY: manifest
@@ -76,17 +78,17 @@ endif
 	cat $(F)/build/json-buildinfo >> build/manifest.json
 	echo -n -e "\r\n}\r\n" >> build/manifest.json
 
-all: $(COMPONENT_PATH)/../build/pack.img
+all: $(LWS_BUILD_PATH)/pack.img
 
-flash: $(COMPONENT_PATH)/../build/pack.img
+flash: $(LWS_BUILD_PATH)/pack.img
 
-flash_ota: $(COMPONENT_PATH)/../build/pack.img
+flash_ota: $(LWS_BUILD_PATH)/pack.img
 	DIRNAME=$$(basename $$(pwd) | tr -d '\n') ;\
 	$(IDF_PATH)/components/esptool_py/esptool/esptool.py \
 		--chip esp32 \
 		--port $(ESPPORT) \
 		--baud $(CONFIG_ESPTOOLPY_BAUD) \
-		write_flash 0x110000 $(COMPONENT_PATH)/../build/$$DIRNAME.bin
+		write_flash 0x110000 $(LWS_BUILD_PATH)/$$DIRNAME.bin
 
 erase_ota:
 	$(IDF_PATH)/components/esptool_py/esptool/esptool.py \
