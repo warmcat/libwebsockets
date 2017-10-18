278e52047989102865196ca8a1d14a0e5e488398
Andy Green <andy@warmcat.com>
2017-10-18 19:27:38 +0800

    JWK + JWS: JSON Web Keys RFC7517 + Signatures support RFC7515
    This adds some optional apis around JSON Web Keys and JSON Web Signatures.
    
    HMAC SHA256/384/512 and RSA + SHA256/384/512 are supported.
    
    This also add a new "LWS_WITH_SELFTESTS" that enables the selftests.

--- lib/private-libwebsockets.h
+++ lib/private-libwebsockets.h
@@ -1221,6 +1221,9 @@ lws_restart_ws_ping_pong_timer(struct lws *wsi);
 struct lws *
 lws_adopt_socket_vhost(struct lws_vhost *vh, lws_sockfd_type accept_fd);
 
+int
+lws_jws_base64_enc(const char *in, size_t in_len, char *out, size_t out_max);
+
 
 enum {
 	LWS_EV_READ = (1 << 0),
@@ -2499,6 +2502,8 @@ lws_ssl_capable_write_no_ssl(struct lws *wsi, unsigned char *buf, int len);
 
 LWS_EXTERN int LWS_WARN_UNUSED_RESULT
 lws_ssl_pending_no_ssl(struct lws *wsi);
+
+int lws_jws_selftest(void);
 
 #ifdef LWS_WITH_HTTP_PROXY
 struct lws_rewrite {
