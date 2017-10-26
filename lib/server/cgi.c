52d82ad83a3acf342858f463f3a05eed3ceab4de
Andy Green <andy@warmcat.com>
2017-10-26 18:54:52 +0800

    cgi: make sure compiler cant see uninitialized use

--- lib/server/cgi.c
+++ lib/server/cgi.c
@@ -116,7 +116,7 @@ lws_cgi(struct lws *wsi, const char * const *exec_array, int script_uri_path_len
 	char *env_array[30], cgi_path[400], e[1024], *p = e,
 	     *end = p + sizeof(e) - 1, tok[256], *t;
 	struct lws_cgi *cgi;
-	int n, m, i, uritok = -1;
+	int n, m = 0, i, uritok = -1;
 
 	/*
 	 * give the master wsi a cgi struct
