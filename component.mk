3db1ce095357ec7d534d8e38898c7409806953d5
Ben Delarre <ben@delarre.net>
2017-11-11 17:18:59 -0800

    Esp32: allow build on windows

--- component.mk
+++ component.mk
@@ -1,10 +1,18 @@
-COMPONENT_DEPENDS:=mbedtls openssl
-COMPONENT_ADD_INCLUDEDIRS := ../../../../../../../../../../../../../../../../../../../../$(COMPONENT_BUILD_DIR)/include
+COMPONENT_DEPENDS := mbedtls openssl
+#COMPONENT_ADD_INCLUDEDIRS := ../../../../../../../../../../../../../../../../../../../../$(COMPONENT_BUILD_DIR)/include
 
-COMPONENT_OWNBUILDTARGET:= 1
+COMPONENT_OWNBUILDTARGET := 1
 
-CROSS_PATH1:=$(shell which xtensa-esp32-elf-gcc )
-CROSS_PATH:= $(shell dirname $(CROSS_PATH1) )/..
+CROSS_PATH1 := $(shell which xtensa-esp32-elf-gcc )
+CROSS_PATH := $(shell dirname $(CROSS_PATH1) )/..
+
+# detect MSYS2 environment and set generator flag if found
+# also set executable extension to .exe so that tools can be properly found
+# and disable bundled zlib
+MSYS_VERSION = $(if $(findstring Msys, $(shell uname -o)),$(word 1, $(subst ., ,$(shell uname -r))),0)
+ifneq ($(MSYS_VERSION),0)
+	MSYS_FLAGS = -DLWS_WITH_BUNDLED_ZLIB=0 -DEXECUTABLE_EXT=.exe -G'MSYS Makefiles'
+endif
 
 # -DNDEBUG=1 after cflags stops debug etc being built
 .PHONY: build
@@ -20,9 +28,11 @@ build:
 		-DLWS_MBEDTLS_INCLUDE_DIRS="${IDF_PATH}/components/openssl/include;${IDF_PATH}/components/mbedtls/include;${IDF_PATH}/components/mbedtls/port/include" \
 		-DLWS_WITH_STATS=0 \
 		-DLWS_WITH_HTTP2=1 \
+		-DLWS_WITH_ACME=1 \
 		-DZLIB_LIBRARY=$(BUILD_DIR_BASE)/zlib/libzlib.a \
 		-DZLIB_INCLUDE_DIR=$(COMPONENT_PATH)/../zlib \
-		-DLWS_WITH_ESP32=1 ;\
+		-DLWS_WITH_ESP32=1 \
+		$(MSYS_FLAGS) ; \
 	make && \
 	cp ${COMPONENT_BUILD_DIR}/lib/libwebsockets.a ${COMPONENT_BUILD_DIR}/liblibwebsockets.a
 
@@ -30,6 +40,3 @@ clean: myclean
 
 myclean:
 	rm -rf ./build
-
-INCLUDES := $(INCLUDES) -I build/ 
-
