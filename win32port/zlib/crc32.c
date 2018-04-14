5dab4b11236e4a0e7fecad329c89ee66774318e1
Mark Adler <madler@alumni.caltech.edu>
2018-04-14 13:06:19 +0800

    zlib: Avoid pre-decrement of pointer in big-endian CRC calculation
    There was a small optimization for PowerPCs to pre-increment a
    pointer when accessing a word, instead of post-incrementing. This
    required prefacing the loop with a decrement of the pointer,
    possibly pointing before the object passed. This is not compliant
    with the C standard, for which decrementing a pointer before its
    allocated memory is undefined. When tested on a modern PowerPC
    with a modern compiler, the optimization no longer has any effect.
    Due to all that, and per the recommendation of a security audit of
    the zlib code by Trail of Bits and TrustInSoft, in support of the
    Mozilla Foundation, this "optimization" was removed, in order to
    avoid the possibility of undefined behavior.
    
    https://github.com/madler/zlib/commit/d1d577490c15a0c6862473d7576352a9f18ef811.patch

--- win32port/zlib/crc32.c
+++ win32port/zlib/crc32.c
@@ -295,7 +295,7 @@ local unsigned long crc32_little(crc, buf, len)
 }
 
 /* ========================================================================= */
-#define DOBIG4 c ^= *++buf4; \
+#define DOBIG4 c ^= *buf4++; \
         c = crc_table[4][c & 0xff] ^ crc_table[5][(c >> 8) & 0xff] ^ \
             crc_table[6][(c >> 16) & 0xff] ^ crc_table[7][c >> 24]
 #define DOBIG32 DOBIG4; DOBIG4; DOBIG4; DOBIG4; DOBIG4; DOBIG4; DOBIG4; DOBIG4
@@ -317,7 +317,6 @@ local unsigned long crc32_big(crc, buf, len)
     }
 
     buf4 = (const u4 FAR *)(const void FAR *)buf;
-    buf4--;
     while (len >= 32) {
         DOBIG32;
         len -= 32;
@@ -326,7 +325,6 @@ local unsigned long crc32_big(crc, buf, len)
         DOBIG4;
         len -= 4;
     }
-    buf4++;
     buf = (const unsigned char FAR *)buf4;
 
     if (len) do {
