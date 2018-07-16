2c30d16f52f9fc49c7d4062ded82100082719f67
Dmytro Ivanov <jimon.j1m0n@gmail.com>
2018-07-16 11:45:55 +0200

    Implement client_ssl_ca_mem/client_ssl_ca_mem_len to load root CA certs from memory.

--- lib/tls/mbedtls/mbedtls-client.c
+++ lib/tls/mbedtls/mbedtls-client.c
@@ -188,6 +188,8 @@ lws_tls_client_create_vhost_context(struct lws_vhost *vh,
 				    const struct lws_context_creation_info *info,
 				    const char *cipher_list,
 				    const char *ca_filepath,
+				    const void *ca_mem,
+				    unsigned int ca_mem_len,
 				    const char *cert_filepath,
 				    const char *private_key_filepath)
 {
@@ -214,16 +216,20 @@ lws_tls_client_create_vhost_context(struct lws_vhost *vh,
 		return 1;
 	}
 
-	if (!ca_filepath)
+	if (!ca_filepath && (!ca_mem || !ca_mem_len))
 		return 0;
 
-	if (alloc_file(vh->context, ca_filepath, &buf, &len)) {
-		lwsl_err("Load CA cert file %s failed\n", ca_filepath);
-		return 1;
+	if (ca_filepath) {
+		if (alloc_file(vh->context, ca_filepath, &buf, &len)) {
+			lwsl_err("Load CA cert file %s failed\n", ca_filepath);
+			return 1;
+		}
+		vh->tls.x509_client_CA = d2i_X509(NULL, buf, len);
+		free(buf);
+	} else {
+		vh->tls.x509_client_CA = d2i_X509(NULL, (uint8_t*)ca_mem, ca_mem_len);
 	}
 
-	vh->tls.x509_client_CA = d2i_X509(NULL, buf, len);
-	free(buf);
 	if (!vh->tls.x509_client_CA) {
 		lwsl_err("client CA: x509 parse failed\n");
 		return 1;
