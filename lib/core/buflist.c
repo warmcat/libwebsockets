20923db2b56d4a2800a18e46f31c06a2ad9359a4
Andy Green <andy@warmcat.com>
2019-08-07 08:11:04 +0100

    COVA14455: unused var init

--- lib/core/buflist.c
+++ lib/core/buflist.c
@@ -52,6 +52,7 @@ lws_buflist_append_segment(struct lws_buflist **head, const uint8_t *buf,
 		head = &((*head)->next);
 	}
 
+	(void)p;
 	lwsl_info("%s: len %u first %d %p\n", __func__, (unsigned int)len,
 					      first, p);
 
