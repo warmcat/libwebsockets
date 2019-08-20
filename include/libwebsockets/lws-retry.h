b5c2cb954b5329c82f2232c56f4655573a7a1736
Andy Green <andy@warmcat.com>
2019-08-20 04:49:30 +0100

    lws_retry: use base ms number and set jitter percentage

--- include/libwebsockets/lws-retry.h
+++ include/libwebsockets/lws-retry.h
@@ -22,28 +22,18 @@
  * IN THE SOFTWARE.
  */
 
-/*
- * Specifies backoff ranges using a pair of uint32_t in ms for the min, max.
- *
- * The actual backoff timing is picked randomly within the range.
- */
-
-typedef struct lws_retry_range {
-	uint32_t		min_ms;
-	uint32_t		max_ms;
-} lws_retry_range_t;
-
 typedef struct lws_retry_bo {
-	const lws_retry_range_t	*retry_ms_table;        /* backoff range pair */
-	uint16_t		retry_ms_table_count;      /* ranges in table */
+	const uint32_t		*retry_ms_table;	  /* base delay in ms */
+	uint16_t		retry_ms_table_count;     /* entries in table */
 	uint16_t		conceal_count;	    /* max retries to conceal */
+	uint8_t			jitter_percent; /* % additional random jitter */
 } lws_retry_bo_t;
 
 /**
  * lws_retry_get_delay_ms() - get next delay from backoff table
  *
  * \param lws_context: the lws context (used for getting random)
- * \param retry: the retry backoff table we are using
+ * \param retry: the retry backoff table we are using, or NULL for default
  * \param ctry: pointer to the try counter
  * \param conceal: pointer to flag set to nonzero if the try should be concealed
  *			in terms of creating an error
@@ -53,9 +43,12 @@ typedef struct lws_retry_bo {
  * set if the number of tries is less than the backoff table conceal_count, or
  * is zero if it exceeded it.  This lets you conceal a certain number of retries
  * before alerting the caller there is a problem.
+ *
+ * If \p retry is NULL, a default of 3s + (0..300ms jitter) is used.  If it's
+ * non-NULL but jitter_percent is 0, the default of 30% jitter is retained.
  */
 
 LWS_VISIBLE LWS_EXTERN unsigned int
 lws_retry_get_delay_ms(struct lws_context *context, const lws_retry_bo_t *retry,
-		        uint16_t *ctry, char *conceal);
+		       uint16_t *ctry, char *conceal);
 
