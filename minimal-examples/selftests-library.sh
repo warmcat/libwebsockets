bca993f7ed08e27083ea76311e5c717363a2760c
Andy Green <andy@warmcat.com>
2019-11-28 05:23:50 +0000

    ntpclient: update for udp adopt interface binding
    https://github.com/warmcat/libwebsockets/issues/1781
    
    Build this in travis so I could find this earlier

--- minimal-examples/selftests-library.sh
+++ minimal-examples/selftests-library.sh
@@ -57,7 +57,7 @@ dotest() {
 	T=$3
 	(
 		{
-			/usr/bin/time -p $1/lws-$MYTEST $4 $5 $6 $7 $8 $9 > $2/$MYTEST/$T.log 2> $2/$MYTEST/$T.log ;
+			/usr/bin/time -p /usr/bin/valgrind $1/lws-$MYTEST $4 $5 $6 $7 $8 $9 > $2/$MYTEST/$T.log 2> $2/$MYTEST/$T.log ;
 			echo $? > $2/$MYTEST/$T.result
 		} 2> $2/$MYTEST/$T.time >/dev/null
 	) >/dev/null 2> /dev/null &
--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -8,7 +8,7 @@ then
 
 	if [ "$LWS_METHOD" == "lwsws" -o "$LWS_METHOD" == "lwsws2" ];
 	then
-		sudo apt-get install -y -qq realpath libjemalloc1 libev4 libuv-dev libdbus-1-dev
+		sudo apt-get install -y -qq realpath libjemalloc1 libev4 libuv-dev libdbus-1-dev valgrind
 		sudo apt-get remove python-six
 		sudo pip install "six>=1.9"
 		sudo pip install "Twisted==16.0.0"
@@ -23,7 +23,7 @@ then
 
 	if [ "$LWS_METHOD" == "mbedtls" ];
 	then
-		sudo apt-get install -y -qq realpath libjemalloc1 libev4 libuv-dev
+		sudo apt-get install -y -qq realpath libjemalloc1 libev4 libuv-dev valgrind
 		wget https://libwebsockets.org/openssl-1.1.0-trusty.tar.bz2 -O/tmp/openssl.tar.bz2
 		cd /
 		sudo tar xf /tmp/openssl.tar.bz2
