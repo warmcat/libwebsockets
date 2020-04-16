e4c59e3a91e41f516db49ded8e58993d66026cae
Andy Green <andy@warmcat.com>
2020-04-16 14:16:29 +0100

    osx: mbedtls: SYSCALL and errno 35 is WANT_READ

--- lib/tls/mbedtls/mbedtls-server.c
+++ lib/tls/mbedtls/mbedtls-server.c
@@ -332,6 +332,11 @@ lws_tls_server_accept(struct lws *wsi)
 	if (m == SSL_ERROR_SYSCALL && errno == 11)
 		return LWS_SSL_CAPABLE_MORE_SERVICE_READ;
 
+#if defined(__APPLE__)
+	if (m == SSL_ERROR_SYSCALL && errno == 35)
+		return LWS_SSL_CAPABLE_MORE_SERVICE_READ;
+#endif
+
 #if defined(WIN32)
 	if (m == SSL_ERROR_SYSCALL && errno == 0)
 		return LWS_SSL_CAPABLE_MORE_SERVICE_READ;
