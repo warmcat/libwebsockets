c9e698ab025b9c0ba15f59bc022dc04c7203b31f
Andy Green <andy@warmcat.com>
2020-06-26 09:58:20 +0100

    client: suppress CONNECTION_ERROR if still in client_connect_via_info
    If the client connection attempt fails early, we report it will a NULL
    return from the client connection api.  If it fails later, perhaps after
    more times around the event loop, we report it as a CONNECTION_ERROR.
    
    This patch makes sure we don't do CONNECTION_ERROR flow if in fact we
    are still in the client_connect_via_info() and in a position to report
    the failure by returning NULL from there, without it under some
    conditions we will do both a CONNECTION_ERROR and return NULL.

--- lib/core-net/connect.c
+++ lib/core-net/connect.c
@@ -58,7 +58,11 @@ lws_client_connect_via_info(const struct lws_client_connect_info *i)
 	if (wsi == NULL)
 		goto bail;
 
-
+	/*
+	 * Until we exit, we can report connection failure directly to the
+	 * caller without needing to call through to protocol CONNECTION_ERROR.
+	 */
+	wsi->client_suppress_CONNECTION_ERROR = 1;
 
 	wsi->context = i->context;
 	wsi->desc.sockfd = LWS_SOCK_INVALID;
@@ -349,8 +353,15 @@ lws_client_connect_via_info(const struct lws_client_connect_info *i)
 
 		/* fallthru */
 
-		lws_http_client_connect_via_info2(wsi);
+		wsi = lws_http_client_connect_via_info2(wsi);
 	}
+
+	if (wsi)
+		/*
+		 * If it subsequently fails, report CONNECTION_ERROR,
+		 * because we're going to return a non-error return now.
+		 */
+		wsi->client_suppress_CONNECTION_ERROR = 0;
 
 	return wsi;
 
