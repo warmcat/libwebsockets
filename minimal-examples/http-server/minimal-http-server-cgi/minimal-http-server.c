62c328244cb59fb6388f7e7a82f4ada850ea83e9
Andy Green <andy@warmcat.com>
2020-08-20 07:03:02 +0100

    cmake: fix things ignoring LWS_WITH_TLS

--- minimal-examples/http-server/minimal-http-server-cgi/minimal-http-server.c
+++ minimal-examples/http-server/minimal-http-server-cgi/minimal-http-server.c
@@ -82,11 +82,13 @@ int main(int argc, const char **argv)
 	info.options =
 		LWS_SERVER_OPTION_HTTP_HEADERS_SECURITY_BEST_PRACTICES_ENFORCE;
 
+#if defined(LWS_WITH_TLS)
 	if (lws_cmdline_option(argc, argv, "-s")) {
 		info.options |= LWS_SERVER_OPTION_DO_SSL_GLOBAL_INIT;
 		info.ssl_cert_filepath = "localhost-100y.cert";
 		info.ssl_private_key_filepath = "localhost-100y.key";
 	}
+#endif
 
 	context = lws_create_context(&info);
 	if (!context) {
