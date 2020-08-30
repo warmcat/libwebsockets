932f3403b8ca16611e5f73cc23bb12d9b72c9ad5
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2020-08-30 14:35:27 +0300

    openssl: allow build with old glibc headers
    strnlen requires _GNU_SOURCE prior to glibc 2.10.

--- lib/tls/openssl/openssl-client.c
+++ lib/tls/openssl/openssl-client.c
@@ -21,6 +21,13 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
+#if defined LWS_HAVE_X509_VERIFY_PARAM_set1_host
+#include <features.h>
+/* Before glibc 2.10, strnlen required _GNU_SOURCE */
+#if __GLIBC__ < 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ < 10)
+#define _GNU_SOURCE
+#endif
+#endif
 #include <string.h>
 
 #include "private-lib-core.h"
