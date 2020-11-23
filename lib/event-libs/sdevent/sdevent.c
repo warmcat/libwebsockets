8dc7679f5e0ce0654ed9a9541083295260f43da4
Christian Fuchs <cfuchs@scs.ch>
2020-11-23 13:41:45 +0100

    impl rudimentary init_pt_sd (init_pt for sd-event)

--- lib/event-libs/sdevent/sdevent.c
+++ lib/event-libs/sdevent/sdevent.c
@@ -1,4 +1,6 @@
 #include <pthread.h>
+#include <systemd/sd-event.h>
+
 #include <private-lib-core.h>
 #include "private-lib-event-libs-sdevent.h"
 
@@ -9,6 +11,8 @@ struct lws_context_eventlibs_sdevent {
 };
 
 struct lws_pt_eventlibs_sdevent {
+    struct lws_context_per_thread *pt;
+    struct sd_event *io_loop;
 };
 
 struct lws_vh_eventlibs_sdevent {
@@ -47,12 +51,46 @@ static int init_vhost_listen_wsi_sd(struct lws *wsi) {
 }
 
 static int init_pt_sd(struct lws_context *context, void *_loop, int tsi) {
-    pthread_t pt_id = pthread_self();
-    printf("%s(%d) [%lu] %s not implemented\n", __FILE__, __LINE__, pt_id, __func__);
 
-    // extra info, can be removed
-    printf("%s(%d) %s _loop is %p\n", __FILE__, __LINE__, __func__, _loop);
-    printf("%s(%d) %s tsi is %d\n", __FILE__, __LINE__, __func__, tsi);
+    struct lws_context_per_thread *pt = &context->pt[tsi];
+    struct lws_pt_eventlibs_sdevent *ptpriv = pt_to_priv_sd(pt);
+
+    struct sd_event *loop = (struct sd_event *)_loop;
+    int first = 1;  // we are the first that create and initialize the loop
+
+    ptpriv->pt = pt;
+
+    // make sure we have an event loop
+    if (!ptpriv->io_loop) {
+        if (!loop) {
+            if (sd_event_default(&loop) < 0) {
+                lwsl_err("cannot get/create event loop, possibly out-of-memory\n");
+                return -1;
+            }
+            pt->event_loop_foreign = 0;
+        } else {
+            pt->event_loop_foreign = 1;
+        }
+
+        ptpriv->io_loop = loop;
+
+        // TODO for full libwebsockets support, add handling for  LWS_SERVER_OPTION_UV_NO_SIGSEGV_SIGFPE_SPIN
+
+    } else {
+        first = 0;  // the loop was initialized before, we do not need to do full initialization
+    }
+
+    // initialize accept/read for vhosts
+    for (struct lws_vhost *vh = context->vhost_list; vh; vh = vh->vhost_next) {
+        // call lws_event_loop_ops->init_vhost_listen_wsi
+        if (init_vhost_listen_wsi_sd(vh->lserv_wsi) == -1) {
+            return -1;
+        }
+    }
+
+    if (first) {
+        // TODO init sultimer
+    }
 
     return 0;
 }
@@ -72,7 +110,6 @@ static int check_client_connect_ok_sd(struct lws *wsi) {
 static void close_handle_manually_sd(struct lws *wsi) {
     pthread_t pt_id = pthread_self();
     printf("%s(%d) [%lu] %s not implemented\n", __FILE__, __LINE__, pt_id, __func__);
-    return;
 }
 
 static int sock_accept_sd(struct lws *wsi) {
@@ -88,27 +125,22 @@ static void io_sd(struct lws *wsi, int flags) {
     // extra info, can be removed
     printf("%s(%d) %s wsi is %p\n", __FILE__, __LINE__, __func__, wsi);
     printf("%s(%d) %s flags is %x\n", __FILE__, __LINE__, __func__, flags);
-
-    return;
 }
 
 static void run_pt_sd(struct lws_context *context, int tsi) {
     pthread_t pt_id = pthread_self();
     printf("%s(%d) [%lu] %s not implemented\n", __FILE__, __LINE__, pt_id, __func__);
     sleep(1);
-    return;
 }
 
 static void destroy_pt_sd(struct lws_context *context, int tsi) {
     pthread_t pt_id = pthread_self();
     printf("%s(%d) [%lu] %s not implemented\n", __FILE__, __LINE__, pt_id, __func__);
-    return;
 }
 
 static void destroy_wsi_sd(struct lws *wsi) {
     pthread_t pt_id = pthread_self();
     printf("%s(%d) [%lu] %s not implemented\n", __FILE__, __LINE__, pt_id, __func__);
-    return;
 }
 
 const struct lws_event_loop_ops event_loop_ops_sdevent = {
