80ca71d59886a9ed2535a4d0b7e0e44af373eac0
Catalin <cra@mega.nz>
2021-04-27 14:52:38 +0300

    Remove incorrect session ref count increment

--- lib/tls/openssl/openssl-session.c
+++ lib/tls/openssl/openssl-session.c
@@ -399,11 +399,6 @@ lws_tls_session_dump_load(struct lws_vhost *vh, const char *host, uint16_t port,
 		goto bail;
 	}
 
-#if defined(LWS_HAVE_SSL_SESSION_up_ref)
-	SSL_SESSION_up_ref(sess);
-#else
-	lwsl_err("%s: openssl is too old\n", __func__);
-#endif
 	lwsl_tlssess("%s: session loaded OK\n", __func__);
 
 	lws_vhost_unlock(vh); /* } vh --------------  */
