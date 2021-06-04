07bef6b83c2dc187ef9cf3fc33ded1ea1ac93be4
Andy Green <andy@warmcat.com>
2021-06-04 14:57:49 +0100

    coverity: ntp from blob: handle blob missing

--- lib/plat/unix/unix-misc.c
+++ lib/plat/unix/unix-misc.c
@@ -127,9 +127,12 @@ lws_plat_ntpclient_config(struct lws_context *context)
 	char *ntpsrv = getenv("LWS_NTP_SERVER");
 
 	if (ntpsrv && strlen(ntpsrv) < 64) {
-		lws_system_blob_direct_set(lws_system_get_blob(context,
-					    LWS_SYSBLOB_TYPE_NTP_SERVER, 0),
-					    (const uint8_t *)ntpsrv,
+		lws_system_blob_t *blob = lws_system_get_blob(context,
+                                            LWS_SYSBLOB_TYPE_NTP_SERVER, 0);
+		if (!blob)
+			return 0;
+
+		lws_system_blob_direct_set(blob, (const uint8_t *)ntpsrv,
 					    strlen(ntpsrv));
 		return 1;
 	}
