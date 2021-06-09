1c935dff79874f4c147018a330dcba07248a56c3
Chunho Lee <chunhole@amazon.com>
2021-06-09 08:10:22 +0100

    clean: else became a NOP

--- lib/core/context.c
+++ lib/core/context.c
@@ -1377,7 +1377,7 @@ lws_create_context(const struct lws_context_creation_info *info)
 			lwsl_err("%s: policy set failed\n", __func__);
 			goto bail_libuv_aware;
 		}
-	} else
+	}
 #else
 	if (context->pss_policies) {
 		/* user code set the policy objects directly, no parsing step */
@@ -1387,9 +1387,8 @@ lws_create_context(const struct lws_context_creation_info *info)
 			lwsl_err("%s: policy set failed\n", __func__);
 			goto bail_libuv_aware;
 		}
-	} //else
+	}
 #endif
-	//	lws_create_vhost(context, info);
 #endif
 
 	lws_context_init_extensions(info, context);
