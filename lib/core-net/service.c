ea4d8008097846e615b42d0d53a7e2246cf7112d
Andy Green <andy@warmcat.com>
2021-06-24 16:21:11 +0100

    service: fix casts for EXTERNAL_POLL

--- lib/core-net/service.c
+++ lib/core-net/service.c
@@ -303,9 +303,9 @@ lws_service_adjust_timeout(struct lws_context *context, int timeout_ms, int tsi)
 	{
 		lws_usec_t u = __lws_sul_service_ripe(pt->pt_sul_owner,
 				      LWS_COUNT_PT_SUL_OWNERS, lws_now_usecs());
-		if (u < timeout_ms * 1000)
-			timeout_ms = u / 1000;
-	}
+		if (u < (lws_usec_t)timeout_ms * (lws_usec_t)1000)
+			timeout_ms = (int)(u / 1000);
+        }
 #endif
 
 	/*
