2b75c6fb89e6f09d73ca5095950ef8b2d75602ea
Andy Green <andy@warmcat.com>
2021-08-12 08:25:22 +0100

    smd: avoid recursive lock when unregistering during delivery

--- lib/system/smd/smd.c
+++ lib/system/smd/smd.c
@@ -653,10 +653,12 @@ lws_smd_unregister(struct lws_smd_peer *pr)
 {
 	lws_smd_t *smd = lws_container_of(pr->list.owner, lws_smd_t, owner_peers);
 
-	lws_mutex_lock(smd->lock_peers); /* +++++++++++++++++++++++++++ peers */
+	if (!smd->delivering)
+		lws_mutex_lock(smd->lock_peers); /* +++++++++++++++++++ peers */
 	lwsl_cx_notice(pr->ctx, "destroying peer %p", pr);
 	_lws_smd_peer_destroy(pr);
-	lws_mutex_unlock(smd->lock_peers); /* ------------------------- peers */
+	if (!smd->delivering)
+		lws_mutex_unlock(smd->lock_peers); /* ----------------- peers */
 }
 
 int
