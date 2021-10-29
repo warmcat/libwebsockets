4935fe9e1e4328e79717ea3cc402cae91ad6d8c0
Andy Green <andy@warmcat.com>
2021-10-29 15:58:08 +0100

    openssl: remove lws_ssl_get_error_string as cruft
    It's not exported, it seems nothing wants it any more

--- lib/tls/openssl/openssl-tls.c
+++ lib/tls/openssl/openssl-tls.c
@@ -31,45 +31,6 @@ extern int openssl_websocket_private_data_index,
 static char openssl_ex_indexes_acquired;
 #endif
 
-char* lws_ssl_get_error_string(int status, int ret, char *buf, size_t len) {
-	switch (status) {
-	case SSL_ERROR_NONE:
-		return lws_strncpy(buf, "SSL_ERROR_NONE", len);
-	case SSL_ERROR_ZERO_RETURN:
-		return lws_strncpy(buf, "SSL_ERROR_ZERO_RETURN", len);
-	case SSL_ERROR_WANT_READ:
-		return lws_strncpy(buf, "SSL_ERROR_WANT_READ", len);
-	case SSL_ERROR_WANT_WRITE:
-		return lws_strncpy(buf, "SSL_ERROR_WANT_WRITE", len);
-	case SSL_ERROR_WANT_CONNECT:
-		return lws_strncpy(buf, "SSL_ERROR_WANT_CONNECT", len);
-	case SSL_ERROR_WANT_ACCEPT:
-		return lws_strncpy(buf, "SSL_ERROR_WANT_ACCEPT", len);
-	case SSL_ERROR_WANT_X509_LOOKUP:
-		return lws_strncpy(buf, "SSL_ERROR_WANT_X509_LOOKUP", len);
-	case SSL_ERROR_SYSCALL:
-		switch (ret) {
-                case 0:
-                        lws_snprintf(buf, len, "SSL_ERROR_SYSCALL: EOF");
-                        return buf;
-                case -1:
-#ifndef LWS_PLAT_OPTEE
-			lws_snprintf(buf, len, "SSL_ERROR_SYSCALL: %s",
-				     strerror(errno));
-#else
-			lws_snprintf(buf, len, "SSL_ERROR_SYSCALL: %d", errno);
-#endif
-			return buf;
-                default:
-                        return strncpy(buf, "SSL_ERROR_SYSCALL", len);
-	}
-	case SSL_ERROR_SSL:
-		return "SSL_ERROR_SSL";
-	default:
-		return "SSL_ERROR_UNKNOWN";
-	}
-}
-
 void
 lws_tls_err_describe_clear(void)
 {
