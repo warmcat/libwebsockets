8c0e8206b8a8058128dc11d2a0c79a9bd1c35aed
Harshit Malpani <harshit.malpani@espressif.com>
2022-04-21 14:28:10 +0530

    plat: esp32: Remove esp_attr.h file

--- lib/plat/freertos/private-lib-plat-freertos.h
+++ lib/plat/freertos/private-lib-plat-freertos.h
@@ -56,11 +56,15 @@
  #include "FreeRTOS_IP.h"
 #endif
  #include "timers.h"
+#if defined(LWS_ESP_PLATFORM)
  #include <esp_attr.h>
+#endif
  #include <semphr.h>
 #else
  #include "freertos/timers.h"
+#if defined(LWS_ESP_PLATFORM)
  #include <esp_attr.h>
+#endif
  #include <esp_system.h>
  #include <esp_task_wdt.h>
 #endif
