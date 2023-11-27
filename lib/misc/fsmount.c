b391e1141abdb1f46621a67a8f00f0884bf6595a
Philippe Ombredanne <pombredanne@nexb.com>
2023-11-27 09:57:26 +0100

    License: Fix typo in MIT text in fsmount.c
    Somehow, a string replacement of ns by fsm had damaged
    the license text and introduced garbage.
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- lib/misc/fsmount.c
+++ lib/misc/fsmount.c
@@ -6,12 +6,12 @@
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
  * deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicefsme, and/or
- * sell copies of the Software, and to permit persofsm to whom the Software is
- * furnished to do so, subject to the following conditiofsm:
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portiofsm of the Software.
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
