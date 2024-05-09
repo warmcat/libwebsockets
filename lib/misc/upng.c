ec80ede88b714e3e3cbc5f5ff7553c68b2c2aa36
iwashiira <89283357+iwashiira@users.noreply.github.com>
2024-05-09 12:44:14 +0000

    check ims > info_size

--- lib/misc/upng.c
+++ lib/misc/upng.c
@@ -483,6 +483,10 @@ lws_upng_decode(lws_upng_t* u, const uint8_t **_pos, size_t *_size)
 		case UOF_CHUNK_LEN:
 			if (!u->inf.out) {
 				size_t ims = (u->u.bypl * 2) + u->inf.info_size;
+                if (u->inf.info_size > ims) {
+                    lwsl_err("%s: integer overflow occur in ims %lu", __func__, ims);
+                    return LWS_SRET_FATAL + 27;
+                }
 
 				if (u->hold_at_metadata)
 					return LWS_SRET_AWAIT_RETRY;
