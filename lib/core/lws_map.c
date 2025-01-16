5a34404b1ada805b884813ce3ed1c345d4d56c1e
Andy Green <andy@warmcat.com>
2025-01-16 10:34:53 +0000

    coverity 872858: hash gen overflow false positive
    Help it ignore that we use the MS 7 bits in the next part of the operation
    and discard it in the first part.

--- lib/core/lws_map.c
+++ lib/core/lws_map.c
@@ -88,7 +88,10 @@ lws_map_hash_from_key_default(const lws_map_key_t key, size_t kl)
 	const uint8_t *u = (const uint8_t *)key;
 
 	while (kl--)
-		h = ((((h << 7) | (h >> 25)) + 0xa1b2c3d4) ^ (*u++)) ^ h;
+		h = ((
+			(((h & 0x1fffffff /* coverity */ ) << 7) |
+			 (h >> 25)) +
+		       0xa1b2c3d4) ^ (*u++)) ^ h;
 
 	return h;
 }
