7184f3ccc4a886b04b292451fd96da7c436aae5c
Andy Green <andy@warmcat.com>
2025-04-24 17:24:37 +0100

    ss: allow A_R_FAILED to UNREACHABLE
    It's possible for the ss-api test to fail on retry count and
    then see UNREACHABLE before destruction.
    
    ..................

--- lib/secure-streams/secure-streams.c
+++ lib/secure-streams/secure-streams.c
@@ -142,6 +142,7 @@ const uint32_t ss_state_txn_validity[] = {
 
 	[LWSSSCS_ALL_RETRIES_FAILED]	= (1 << LWSSSCS_CONNECTING) |
 					  (1 << LWSSSCS_TIMEOUT) |
+					  (1 << LWSSSCS_UNREACHABLE) |
 					  (1 << LWSSSCS_DESTROYING),
 
 	[LWSSSCS_QOS_ACK_REMOTE]	= (1 << LWSSSCS_DISCONNECTED) |
