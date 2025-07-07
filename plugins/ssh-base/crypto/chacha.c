04dc49e8d7688003e7fbfba11aaa72517bbac921
Andy Green <andy@warmcat.com>
2025-07-07 14:03:49 +0100

    ssh: set correct lengths for sigma and tau

--- plugins/ssh-base/crypto/chacha.c
+++ plugins/ssh-base/crypto/chacha.c
@@ -59,8 +59,8 @@ typedef struct chacha_ctx chacha_ctx;
   a = PLUS(a,b); d = ROTATE(XOR(d,a), 8); \
   c = PLUS(c,d); b = ROTATE(XOR(b,c), 7);
 
-static const char sigma[16] = "expand 32-byte k";
-static const char tau[16] = "expand 16-byte k";
+static const char sigma[17] = "expand 32-byte k";
+static const char tau[17] = "expand 16-byte k";
 
 void
 chacha_keysetup(chacha_ctx *x,const u8 *k,u32 kbits)
