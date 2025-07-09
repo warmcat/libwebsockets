4b252aeb4d637158424c82fac00785a608f09a07
Andy Green <andy@warmcat.com>
2025-07-09 15:32:23 +0100

    cancel_service: align iterator signedness

--- lib/core-net/vhost.c
+++ lib/core-net/vhost.c
@@ -1109,7 +1109,7 @@ void
 lws_cancel_service(struct lws_context *context)
 {
 	struct lws_context_per_thread *pt = &context->pt[0];
-	short m;
+	unsigned short m;
 
 	if (context->service_no_longer_possible)
 		return;
