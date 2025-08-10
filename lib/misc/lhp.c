3a3e5ac7f56657bdd16e65e083f019662df5a51a
Andy Green <andy@warmcat.com>
2025-08-10 05:21:59 +0100

    lhp: fix spacing definition without font
    https://github.com/warmcat/libwebsockets/issues/3440

--- lib/misc/lhp.c
+++ lib/misc/lhp.c
@@ -245,7 +245,7 @@ lhp_clean_stack(lws_dll2_t *d, void *user)
 	return 0;
 }
 
-static const lws_fx_t c_254= { 2,54000000 }, c_10 = { 10,0 },
+static const lws_fx_t c_254= { 2,54000000 }, c_10 = { 10,0 }, c_0 = { 0, 0 },
 			     c_72 = { 72,0 }, c_6 = { 6,0 }, c_100 = { 100,0 };
 
 /*
@@ -324,6 +324,16 @@ lws_csp_px(const lcsp_atr_t *a, lhp_pstack_t *ps)
 
 	ctx = lws_container_of(ps->list.owner, lhp_ctx_t, stack);
 	f = ps->font;
+
+       /*
+        * We rely on f being non-null, but if it happens to be
+        * NULL, let's return a constant 0
+        */
+
+       if (!f) {
+               *(lws_fx_t *)&a->r = c_0;
+               return &a->r;
+       }
 
 	ref = lhp_prop_axis(a);
 
