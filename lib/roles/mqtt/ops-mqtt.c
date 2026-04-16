d658ba69305d5aa08b8057c69b42a7d72a5e26da
Andy Green <andy@warmcat.com>
2026-04-16 06:21:37 +0100

    h2: mqtt: handle all MORE_SERVICE variants

--- lib/roles/mqtt/ops-mqtt.c
+++ lib/roles/mqtt/ops-mqtt.c
@@ -136,6 +136,8 @@ read:
 				  __func__);
 			return LWS_HPI_RET_PLEASE_CLOSE_ME;
 		case LWS_SSL_CAPABLE_MORE_SERVICE:
+		case LWS_SSL_CAPABLE_MORE_SERVICE_READ:
+		case LWS_SSL_CAPABLE_MORE_SERVICE_WRITE:
 			lwsl_info("SSL Capable more service\n");
 			return LWS_HPI_RET_HANDLED;
 		case LWS_SSL_CAPABLE_ERROR:
