6f36c3a9c6d9e9a929452c010ac1c6e70bef9600
Andy Green <andy@warmcat.com>
2026-05-12 09:24:03 +0100

    Implement lws_stub_request and unprivileged client UDS transport

--- lib/core-net/stub.c
+++ lib/core-net/stub.c
@@ -29,6 +29,17 @@
 
 #if defined(LWS_WITH_STUB)
 
+
+struct lws_stub_req {
+	struct lws_dll2 list;
+	char *tx_buf;
+	size_t tx_len;
+	size_t tx_pos;
+	struct lejp_ctx jctx;
+	signed char (*rx_cb)(struct lejp_ctx *ctx, char reason);
+	void *user;
+};
+
 struct lws_stub_manager {
 	struct lws_context *cx;
 	struct lws_vhost *vh;
@@ -37,6 +48,12 @@ struct lws_stub_manager {
 	struct lws_spawn_piped *lsp;
 
 	const lws_jrpc_method_t *rpc_methods;
+
+	struct lws *wsi_client;
+	struct lws_dll2_owner reqs;
+
+	lws_sorted_usec_list_t sul;
+	uint16_t ctry;
 };
 
 struct lws_stub_manager *
@@ -146,16 +163,170 @@ lws_stub_server_init(const struct lws_stub_config *config, char *secret_out)
 	return 0;
 }
 
-int
-lws_stub_rpc_request(struct lws_stub_manager *mgr,
-		     const char *method,
-		     const char *json_params,
-		     void (*on_response)(struct lws_jrpc_obj *resp, void *user),
-		     void *user)
+static const uint32_t backoff_ms[] = { 100, 250, 500, 1000, 5000 };
+
+static const lws_retry_bo_t stub_retry = {
+	.retry_ms_table			= backoff_ms,
+	.retry_ms_table_count		= LWS_ARRAY_SIZE(backoff_ms),
+	.conceal_count			= 1000,
+	.secs_since_valid_ping		= 300,
+	.secs_since_valid_hangup	= 310,
+	.jitter_percent			= 0,
+};
+
+static int
+lws_stub_client_connect(struct lws_stub_manager *mgr)
 {
-	/* TODO: Implement JRPC client connection and state machine */
-	return -1;
+	struct lws_client_connect_info i;
+
+	memset(&i, 0, sizeof(i));
+	i.context = mgr->cx;
+	i.vhost = mgr->vh;
+
+	/* UNIX domain socket addresses need a '+' prefix */
+	char addr[256];
+	lws_snprintf(addr, sizeof(addr), "+%s", mgr->uds_path);
+	i.address = addr;
+
+	i.port = 0;
+	i.protocol = "lws-stub-client";
+	i.host = i.address;
+	i.origin = i.address;
+	i.opaque_user_data = mgr;
+	i.retry_and_idle_policy = &stub_retry;
+
+	mgr->wsi_client = lws_client_connect_via_info(&i);
+	if (!mgr->wsi_client)
+		return -1;
+
+	return 0;
 }
+
+static void
+stub_retry_cb(lws_sorted_usec_list_t *sul)
+{
+	struct lws_stub_manager *mgr = lws_container_of(sul, struct lws_stub_manager, sul);
+
+	if (!mgr->wsi_client)
+		lws_stub_client_connect(mgr);
+}
+
+static int
+callback_stub_client(struct lws *wsi, enum lws_callback_reasons reason,
+		     void *user, void *in, size_t len)
+{
+	struct lws_stub_manager *mgr = (struct lws_stub_manager *)lws_get_opaque_user_data(wsi);
+
+	switch (reason) {
+	case LWS_CALLBACK_CLIENT_CONNECTION_ERROR:
+		lwsl_err("%s: Client connection failed\n", __func__);
+		mgr->wsi_client = NULL;
+		lws_retry_sul_schedule(mgr->cx, 0, &mgr->sul, &stub_retry, stub_retry_cb, &mgr->ctry);
+		break;
+
+	case LWS_CALLBACK_CLIENT_ESTABLISHED:
+		lwsl_notice("%s: UDS connected to stub\n", __func__);
+		mgr->ctry = 0; /* Reset retry counter on success */
+		lws_callback_on_writable(wsi);
+		break;
+
+	case LWS_CALLBACK_CLIENT_WRITEABLE: {
+		struct lws_dll2 *d = lws_dll2_get_head(&mgr->reqs);
+		if (!d)
+			break;
+
+		struct lws_stub_req *req = lws_container_of(d, struct lws_stub_req, list);
+		if (req->tx_pos < req->tx_len) {
+			int n = lws_write(wsi, (unsigned char *)req->tx_buf + req->tx_pos,
+					  req->tx_len - req->tx_pos, LWS_WRITE_RAW);
+			if (n < 0)
+				return -1;
+			req->tx_pos += (size_t)n;
+		}
+
+		if (req->tx_pos < req->tx_len)
+			lws_callback_on_writable(wsi);
+		break;
+	}
+
+	case LWS_CALLBACK_RAW_RX: {
+		struct lws_dll2 *d = lws_dll2_get_head(&mgr->reqs);
+		if (!d)
+			break; /* Received RX but no active request? */
+
+		struct lws_stub_req *req = lws_container_of(d, struct lws_stub_req, list);
+		if (req->rx_cb) {
+			int m = lejp_parse(&req->jctx, (uint8_t *)in, (int)len);
+			if (m < 0 && m != LEJP_CONTINUE) {
+				lwsl_err("%s: lejp parse failed: %d\n", __func__, m);
+				lws_dll2_remove(&req->list);
+				lws_free(req->tx_buf);
+				lejp_destruct(&req->jctx);
+				lws_free(req);
+			} else if (req->jctx.pst[req->jctx.pst_sp].callback == NULL) {
+				/* If parse complete (or if the callback indicates completion) */
+				/* Actually, we can just rely on LEJPCB_OBJECT_END in the callback */
+			}
+		}
+		break;
+	}
+
+	case LWS_CALLBACK_CLIENT_CLOSED:
+		mgr->wsi_client = NULL;
+		break;
+
+	default:
+		break;
+	}
+
+	return 0;
+}
+
+int
+lws_stub_request(struct lws_stub_manager *mgr,
+		 const char *json,
+		 const char * const *rx_paths,
+		 size_t rx_paths_count,
+		 signed char (*rx_cb)(struct lejp_ctx *ctx, char reason),
+		 void *user)
+{
+	struct lws_stub_req *req = lws_zalloc(sizeof(*req), "stub_req");
+	if (!req)
+		return -1;
+
+	req->rx_cb = rx_cb;
+	req->user = user;
+
+	if (rx_cb)
+		lejp_construct(&req->jctx, rx_cb, user, rx_paths, (uint8_t)rx_paths_count);
+
+	size_t n = strlen(json);
+	req->tx_buf = lws_malloc(n + 1, "stub_req_tx");
+	if (!req->tx_buf) {
+		lws_free(req);
+		return -1;
+	}
+	memcpy(req->tx_buf, json, n);
+	req->tx_len = n;
+
+	lws_dll2_add_tail(&req->list, &mgr->reqs);
+
+	if (!mgr->wsi_client) {
+		lws_stub_client_connect(mgr);
+	} else {
+		lws_callback_on_writable(mgr->wsi_client);
+	}
+
+	return 0;
+}
+
+const struct lws_protocols lws_stub_client_protocol = {
+	"lws-stub-client",
+	callback_stub_client,
+	0,
+	0,
+	0, NULL, 0
+};
 
 void
 lws_stub_destroy(struct lws_stub_manager **_mgr)
@@ -163,6 +334,19 @@ lws_stub_destroy(struct lws_stub_manager **_mgr)
 	struct lws_stub_manager *mgr = *_mgr;
 	if (!mgr)
 		return;
+
+	lws_start_foreach_dll_safe(struct lws_dll2 *, d, d1, mgr->reqs.head) {
+		struct lws_stub_req *req = lws_container_of(d, struct lws_stub_req, list);
+		lws_dll2_remove(d);
+		if (req->tx_buf)
+			lws_free(req->tx_buf);
+		if (req->rx_cb)
+			lejp_destruct(&req->jctx);
+		lws_free(req);
+	} lws_end_foreach_dll_safe(d, d1);
+
+	if (mgr->wsi_client)
+		lws_set_opaque_user_data(mgr->wsi_client, NULL);
 
 	if (mgr->lsp)
 		lws_spawn_piped_kill_child_process(mgr->lsp);
