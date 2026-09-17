36d0fdc67ee40094d89a436b05a033a050533c92
Andy Green <andy@warmcat.com>
2026-09-17 09:30:21 +0000

    tree: untrack in-source cmake leftovers and a stale duplicate header
    Six CTestTestfile.cmake from in-source cmake runs had been git added
    alongside real changes (lib/roles/quic, api-test-lhp-dlo, lhp-browser
    and its plat/unix, minimal-http-client-h3, minimal-secure-streams):
    removed from the index and the pattern ignored, along with
    cmake_install.cmake and CMakeFiles/, so it can't happen again.
    
    lhp-browser/private-lhp-browser.h was an older copy of
    plat/private-lhp-browser.h that nothing includes: deleted.

--- minimal-examples-lowlevel/http-client/lhp-browser/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/libwebsockets/minimal-examples-lowlevel/http-client/lhp-browser
-# Build directory: /home/user/libwebsockets/minimal-examples-lowlevel/http-client/lhp-browser
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(lhp-browser-window-shot "/usr/bin/cmake" "-DTOOL=/home/user/libwebsockets/bin/lws-lhp-browser" "-DHTML=/home/user/libwebsockets/minimal-examples-lowlevel/http-client/lhp-browser/test.html" "-DOUT=/home/user/libwebsockets/minimal-examples-lowlevel/http-client/lhp-browser/window-shot.bmp" "-P" "/home/user/libwebsockets/minimal-examples-lowlevel/http-client/lhp-browser/shot-test.cmake")
-set_tests_properties(lhp-browser-window-shot PROPERTIES  _BACKTRACE_TRIPLES "/home/user/libwebsockets/minimal-examples-lowlevel/http-client/lhp-browser/CMakeLists.txt;62;add_test;/home/user/libwebsockets/minimal-examples-lowlevel/http-client/lhp-browser/CMakeLists.txt;0;")
-subdirs("plat/unix")
